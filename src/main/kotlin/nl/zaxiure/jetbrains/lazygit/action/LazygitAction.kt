3805619ac574c5d36c653b082f27cda7408e3390
Daniël Voorn <8901040+Zaxiure@users.noreply.github.com>
2024-07-23 10:16:13 +0200

    Shouldn't use deprecated stuff (:

--- src/main/kotlin/nl/zaxiure/jetbrains/lazygit/action/LazygitAction.kt
+++ src/main/kotlin/nl/zaxiure/jetbrains/lazygit/action/LazygitAction.kt
@@ -3,18 +3,23 @@ package nl.zaxiure.jetbrains.lazygit.action
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.sh.run.ShRunner
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 
 class LazygitAction : AnAction () {
 
     override fun actionPerformed(e: AnActionEvent) {
+        ApplicationManager.getApplication().getService(ShRunner::class.java).run(e.project!!, "lazygit;exit", e.project?.basePath!!, "Lazygit", true)
         val terminalToolWindowManager = TerminalToolWindowManager.getInstance(e.project!!)
 
-        val terminalWidget = terminalToolWindowManager.createLocalShellWidget(e.project!!.basePath, "Lazygit")
-        terminalWidget.executeCommand("lazygit;exit")
+        val terminalWidget =  terminalToolWindowManager.terminalWidgets.find { terminal -> terminal.terminalTitle.buildTitle() == "Lazygit" }
 
-        val actionManager = ActionManager.getInstance()
-        val action = actionManager.getAction("Terminal.MoveToEditor");
-        actionManager.tryToExecute(action, null, terminalWidget.component, null, true)
+
+        if (terminalWidget != null) {
+            val actionManager = ActionManager.getInstance()
+            val action = actionManager.getAction("Terminal.MoveToEditor");
+            actionManager.tryToExecute(action, null, terminalWidget?.component, null, true)
+        }
     }
 }
