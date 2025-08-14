dca03ce9338927a4bb89ec4b4e841d8c101c4528
MJ Studio <mym0404@gmail.com>
2025-08-14 21:21:47 +0900

    feat: update plugin configuration for new repository
    - Change plugin group to happy.mjstudio.jetbrains.lazygit
    - Update repository URL to mym0404/jetbrains-lazygit
    - Extend compatibility to support wider version range (since build 232)
    - Update Gradle and dependencies to latest versions
    - Configure for JetBrains Marketplace deployment

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,5 +3,12 @@
 # jetbrains-lazygit Changelog
 
 ## [Unreleased]
+
+## [0.0.1] - 2025-08-14
+
 ### Added
+
 - Initial scaffold created from [IntelliJ Platform Plugin Template](https://github.com/JetBrains/intellij-platform-plugin-template)
+
+[Unreleased]: https://github.com/mym0404/jetbrains-lazygit/compare/v0.0.1...HEAD
+[0.0.1]: https://github.com/mym0404/jetbrains-lazygit/commits/v0.0.1
--- src/main/kotlin/happy/mjstudio/jetbrains/lazygit/action/LazygitAction.kt
+++ src/main/kotlin/happy/mjstudio/jetbrains/lazygit/action/LazygitAction.kt
@@ -1,4 +1,4 @@
-package nl.zaxiure.jetbrains.lazygit.action
+package happy.mjstudio.jetbrains.lazygit.action
 
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
@@ -7,18 +7,20 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.sh.run.ShRunner
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 
-class LazygitAction : AnAction () {
-
+class LazygitAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        ApplicationManager.getApplication().getService(ShRunner::class.java).run(e.project!!, "lazygit;exit", e.project?.basePath!!, "Lazygit", true)
+        ApplicationManager
+            .getApplication()
+            .getService(
+                ShRunner::class.java,
+            ).run(e.project!!, "lazygit;exit", e.project?.basePath!!, "Lazygit", true)
         val terminalToolWindowManager = TerminalToolWindowManager.getInstance(e.project!!)
 
-        val terminalWidget =  terminalToolWindowManager.terminalWidgets.find { terminal -> terminal.terminalTitle.buildTitle() == "Lazygit" }
-
+        val terminalWidget = terminalToolWindowManager.terminalWidgets.find { terminal -> terminal.terminalTitle.buildTitle() == "Lazygit" }
 
         if (terminalWidget != null) {
             val actionManager = ActionManager.getInstance()
-            val action = actionManager.getAction("Terminal.MoveToEditor");
+            val action = actionManager.getAction("Terminal.MoveToEditor")
             actionManager.tryToExecute(action, null, terminalWidget?.component, null, true)
         }
     }
